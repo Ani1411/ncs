655f3528f8ea3a504f8d28e8a704fcd45e5448da
Ani1411 <ani261114@gmail.com>
2022-10-28 15:38:09 +0530

    created navbar

---
+++ frontend/src/components/navbar/index.js
@@ -0,0 +1,15 @@
+import './nav.css'
+
+
+const Navbar = () => {
+    return <nav>
+        <div>
+            <h2>STREAMSITE</h2>
+        </div>
+        <div>
+            <span>Account</span>
+        </div>
+    </nav>
+}
+
+export default Navbar;
